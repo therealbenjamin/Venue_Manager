a0570ab45949a40c4e198a50427ce5c2dbaa637e
Ben Blackburn <benjamin.blackburn@gmail.com>
2014-01-24 11:53:08 -0600

    venue update successfully refactored into class method

--- lib/venue.rb
+++ lib/venue.rb
@@ -16,22 +16,6 @@ class Venue
 		@longitude = options[:longitude]
 	end
 
-	def view db
-	name = pull_venue_name
-	record = db.get_venue_by_name(name)
-	clear()
-	puts
-	puts "Details for this venue are as follows:"
-	puts
-	puts red(record[0][1])
-	puts red(record[0][2])
-	puts red(record[0][3])
-	puts red(record[0][4])
-	puts red(record[0][5])
-	puts red(record[0][6])
-	puts
-	end
-
 	def geocode
 	coordinates = Geocoder.coordinates(full_address)
 	@latitude = coordinates[0]
@@ -51,8 +35,56 @@ class Venue
 		VALUES (?, ?, ?, ?, ?, ?)", [@name, @address, @city, @state, @latitude, @longitude])
 	end
 
-	def update db
+	def view db
+		name = pull_venue_name
+		record = db.get_venue_by_name(name)
+		clear()
+		puts
+		puts "Details for this venue are as follows:"
+		puts
+		puts red(record[0][1])
+		puts red(record[0][2])
+		puts red(record[0][3])
+		puts red(record[0][4])
+		puts red(record[0][5])
+		puts red(record[0][6])
+		puts
+	end
 
+	def update db
+		puts red("UPDATE a venue record:")
+		name = pull_venue_name()
+		record = db.get_venue_by_name(name)
+		puts "Details for this venue are as follows:"
+		puts
+		puts red(record[0][1])
+		puts red(record[0][2])
+		puts red(record[0][3])
+		puts red(record[0][4])
+		puts
+		puts "Reset venue details:"
+		puts
+		new_name = pull_venue_name()
+		new_address = pull_venue_address()
+		new_city = pull_venue_city()
+		new_state = pull_venue_state()
+		db.connection.execute("UPDATE venues SET name = '#{new_name}'")
+		db.connection.execute("UPDATE venues SET address = '#{new_address}'")
+		db.connection.execute("UPDATE venues SET city = '#{new_city}'")
+		db.connection.execute("UPDATE venues SET state = '#{new_state}'")
+
+		coordinates = Geocoder.coordinates(new_address + ", " + new_city + ", " + new_state)
+		new_latitude = coordinates[0]
+		new_longitude = coordinates[1]
+
+		puts "Details have been successfully updated to:"
+		new_record = db.get_venue_by_name(new_name)
+		puts red(new_record[0][1])
+		puts red(new_record[0][2])
+		puts red(new_record[0][3])
+		puts red(new_record[0][4])
+		puts red(new_record[0][5])
+		puts red(new_record[0][6])
 	end
 
 	def kill db
